6cf273c310172ae41e4a51afa79a223e846a86a6
Tripp Shelnutt <trippshelnutt@outlook.com>
2021-02-27 21:49:28 -0500

    Extracted functions

--- src/GildedRose.Console/ItemUpdater.cs
+++ src/GildedRose.Console/ItemUpdater.cs
@@ -1,76 +1,97 @@
-﻿namespace GildedRose.Console
+﻿using System;
+
+namespace GildedRose.Console
 {
     public class ItemUpdater
     {
         public void UpdateItem(Item item)
         {
-            if (item.Name != ItemNames.AgedBrieName && item.Name != ItemNames.BackstagePassesName)
+            if (item.Name == ItemNames.AgedBrieName)
             {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != ItemNames.SulfurasName)
-                    {
-                        item.Quality -= 1;
-                    }
-                }
+                UpdateAgedBrie(item);
+            }
+            else if (item.Name == ItemNames.BackstagePassesName)
+            {
+                UpdateBackstagePasses(item);
+            }
+            else if (item.Name == ItemNames.SulfurasName)
+            {
+                UpdateSulfuras(item);
             }
             else
+            {
+                UpdateBasicItem(item);
+            }
+        }
+
+        private void UpdateAgedBrie(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
+
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
             {
                 if (item.Quality < 50)
                 {
                     item.Quality += 1;
-
-                    if (item.Name == ItemNames.BackstagePassesName)
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
-                    }
                 }
             }
+        }
 
-            if (item.Name != ItemNames.SulfurasName)
+        private void UpdateBackstagePasses(Item item)
+        {
+            if (item.Quality < 50)
             {
-                item.SellIn -= 1;
-            }
+                item.Quality += 1;
 
-            if (item.SellIn < 0)
-            {
-                if (item.Name != ItemNames.AgedBrieName)
-                {
-                    if (item.Name != ItemNames.BackstagePassesName)
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != ItemNames.SulfurasName)
-                            {
-                                item.Quality -= 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality -= item.Quality;
-                    }
-                }
-                else
+                if (item.SellIn < 11)
                 {
                     if (item.Quality < 50)
                     {
                         item.Quality += 1;
                     }
+                }
+
+                if (item.SellIn < 6)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality += 1;
+                    }
+                }
+            }
+
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality -= item.Quality;
+            }
+        }
+
+        private void UpdateSulfuras(Item _)
+        {
+            // do nothing
+        }
+
+        private void UpdateBasicItem(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality -= 1;
+            }
+
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality -= 1;
                 }
             }
         }
